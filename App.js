43895a5243cec0ca45ae05767b1fc12f37a8f8e3
WeerawitPM <63030079@kmitl.ac.th>
2022-06-02 18:27:38 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
       <StatusBar style="auto" />
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={ styles.button1 }>
+        <Text style={ styles.text }>เพิ่มไฟล์</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +26,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button1: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#058AFA',
+    elevation: 3,
+    backgroundColor: 'white',
+    width: '85%',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: '#058AFA'
+  }
 });
